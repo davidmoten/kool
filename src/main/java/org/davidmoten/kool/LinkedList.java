b49dd4a27559d1c2fef1db3f922b49f04e36fde5
Dave Moten <davidmoten@gmail.com>
2018-08-23 11:25:19 +1000

    LinkedList dev

--- src/main/java/org/davidmoten/kool/LinkedList.java
+++ src/main/java/org/davidmoten/kool/LinkedList.java
@@ -4,77 +4,175 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 
 import com.github.davidmoten.guavamini.Preconditions;
 
 /**
- * Non-lazy linked list with functional style methods. Aims to be memory and cpu
- * sufficient within the constraints of the advertised data structure.
+ * Non-lazy unidirectional linked list with functional style methods. Aims to be
+ * memory and cpu sufficient within the constraints of the advertised data
+ * structure.
  *
  * @param <T>
  */
 public final class LinkedList<T> implements Iterable<T> {
 
-    private static final Object EMPTY = new Object();
-    private static final LinkedList<Object> NIL = new LinkedList<>(EMPTY, null);
+    private static final int DEFAULT_BUFFER_SIZE = 16;
+    private static final Object HEAD_NOT_PRESENT = new Object();
+    private static final LinkedList<Object> NIL = new LinkedList<>(HEAD_NOT_PRESENT, null);
 
     private final T head;
     private final LinkedList<T> tail;
-    private final int size;
 
     LinkedList(T head, LinkedList<T> tail) {
         Preconditions.checkNotNull(head, "cannot prepend null to a LinkedList");
         this.head = head;
         this.tail = tail;
-        if (head == EMPTY) {
-            this.size = 0;
-        } else {
-            this.size = tail.size() + 1;
-        }
+    }
+
+    public boolean isEmpty() {
+        return this == NIL;
     }
 
     public T head() {
-        return head;
+        if (isEmpty()) {
+            throw new NoSuchElementException("list is empty");
+        } else {
+            return head;
+        }
     }
 
     public LinkedList<T> tail() {
         return tail;
     }
 
-    @SuppressWarnings("unchecked")
-    public static <R> LinkedList<R> nil() {
-        return (LinkedList<R>) NIL;
+    public <R> LinkedList<R> map(F1<T, R> function) {
+        return map(function, 16);
     }
 
-    public <R> LinkedList<R> map(F1<T, R> function) {
+    public <R> LinkedList<R> map(F1<T, R> function, int sizeHint) {
         if (this == NIL) {
             return nil();
         } else {
-            @SuppressWarnings("unchecked")
-            R[] a = (R[]) new Object[size];
+            ArrayList<R> a = new ArrayList<R>(sizeHint);
             {
-                int i = 0;
                 LinkedList<T> x = this;
                 while (x != NIL) {
-                    a[i] = function.apply(x.head);
-                    i++;
+                    a.add(function.apply(x.head));
                     x = x.tail;
                 }
             }
             LinkedList<R> x = nil();
-            for (int i = a.length - 1; i >= 0; i--) {
-                x = x.prepend(a[i]);
+            for (int i = a.size() - 1; i >= 0; i--) {
+                x = x.prepend(a.get(i));
             }
             return x;
         }
+    }
+    
+    public ArrayList<T> toJavaArrayList() {
+        return toJavaArrayList(DEFAULT_BUFFER_SIZE);
+    }
+
+    public ArrayList<T> toJavaArrayList(int sizeHint) {
+        ArrayList<T> a = new ArrayList<>();
+        for (T t : this) {
+            a.add(t);
+        }
+        return a;
+    }
+
+    public LinkedList<T> filter(Predicate<T> function) {
+        return filter(function, DEFAULT_BUFFER_SIZE);
+    }
+
+    public LinkedList<T> filter(Predicate<T> function, int sizeHint) {
+        if (this == NIL) {
+            return nil();
+        } else {
+            ArrayList<T> a = new ArrayList<T>(sizeHint);
+            {
+                LinkedList<T> x = this;
+                while (x != NIL) {
+                    boolean include = function.test(x.head);
+                    if (include) {
+                        a.add(x.head);
+                    }
+                    x = x.tail;
+                }
+            }
+            LinkedList<T> x = nil();
+            for (int i = a.size() - 1; i >= 0; i--) {
+                x = x.prepend(a.get(i));
+            }
+            return x;
+        }
+    }
+
+    /**
+     * Returns the number of elements in the list. O(N) algorithmic complexity.
+     * 
+     * @return the number of elements in the list
+     */
+    public int size() {
+        int size = 0;
+        LinkedList<T> x = this;
+        while (x != NIL) {
+            size++;
+            x = x.tail;
+        }
+        return size;
     }
 
     public LinkedList<T> prepend(T value) {
         return new LinkedList<T>(value, this);
     }
 
-    public int size() {
-        return size;
+    public LinkedList<T> prepend(T[] values) {
+        LinkedList<T> x = this;
+        for (int i = values.length - 1; i >= 0; i--) {
+            x = x.prepend(values[i]);
+        }
+        return x;
+    }
+
+    public LinkedList<T> prepend(List<T> values) {
+        LinkedList<T> x = this;
+        for (int i = values.size() - 1; i >= 0; i--) {
+            x = x.prepend(values.get(i));
+        }
+        return x;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            LinkedList<T> list = LinkedList.this;
+
+            @Override
+            public boolean hasNext() {
+                return list != NIL;
+            }
+
+            @Override
+            public T next() {
+                if (list == NIL) {
+                    throw new NoSuchElementException();
+                } else {
+                    T t = list.head;
+                    list = list.tail;
+                    return t;
+                }
+            }
+        };
+    }
+
+    // factory methods
+
+    @SuppressWarnings("unchecked")
+    public static <R> LinkedList<R> nil() {
+        return (LinkedList<R>) NIL;
     }
 
     public static <T> LinkedList<T> of(T t1) {
@@ -119,46 +217,6 @@ public final class LinkedList<T> implements Iterable<T> {
             a.add(t);
         }
         return LinkedList.<T>nil().prepend(a);
-    }
-
-    public LinkedList<T> prepend(T[] values) {
-        LinkedList<T> x = this;
-        for (int i = values.length - 1; i >= 0; i--) {
-            x = x.prepend(values[i]);
-        }
-        return x;
-    }
-
-    public LinkedList<T> prepend(List<T> values) {
-        LinkedList<T> x = this;
-        for (int i = values.size() - 1; i >= 0; i--) {
-            x = x.prepend(values.get(i));
-        }
-        return x;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-
-            LinkedList<T> list = LinkedList.this;
-
-            @Override
-            public boolean hasNext() {
-                return list != NIL;
-            }
-
-            @Override
-            public T next() {
-                if (list == NIL) {
-                    throw new NoSuchElementException();
-                } else {
-                    T t = list.head;
-                    list = list.tail;
-                    return t;
-                }
-            }
-        };
     }
 
 }
--- src/test/java/org/davidmoten/kool/LinkedListTest.java
+++ src/test/java/org/davidmoten/kool/LinkedListTest.java
@@ -1,21 +1,58 @@
 package org.davidmoten.kool;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.github.davidmoten.guavamini.Lists;
 
 public class LinkedListTest {
 
     @Test
-    public void test() {
+    public void testIterator() {
 
         LinkedList<Integer> list = LinkedList.of(1, 2, 3);
         Iterator<Integer> it = list.iterator();
         assertEquals(1, (int) it.next());
         assertEquals(2, (int) it.next());
+        assertTrue(it.hasNext());
         assertEquals(3, (int) it.next());
+        assertFalse(it.hasNext());
+        try {
+            it.next();
+            Assert.fail();
+        } catch (NoSuchElementException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testNilIsEmpty() {
+        LinkedList<Integer> list = LinkedList.nil();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testMapNil() {
+        LinkedList<Integer> list = LinkedList.nil();
+        assertTrue(list.map(x -> x + 1).isEmpty());
+    }
+
+    @Test
+    public void testToJavaArrayList() {
+        assertEquals(Lists.newArrayList(1, 2, 3), LinkedList.of(1, 2, 3).toJavaArrayList());
+    }
+
+    @Test
+    public void testMap() {
+        LinkedList<Integer> list = LinkedList.of(1, 2, 3);
+        assertEquals(Lists.newArrayList(4, 5, 6), LinkedList.of(1, 2, 3).map(x -> x + 3).toJavaArrayList());
     }
 
 }
