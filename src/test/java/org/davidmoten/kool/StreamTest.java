af58a117beffe05a727aecb6978f67b6ad8f0f9b
Dave Moten <davidmoten@gmail.com>
2018-08-28 17:47:29 +1000

    add Concat test

--- src/test/java/org/davidmoten/kool/StreamTest.java
+++ src/test/java/org/davidmoten/kool/StreamTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Assert;
