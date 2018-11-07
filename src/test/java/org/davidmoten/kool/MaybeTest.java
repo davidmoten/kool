89c82068b4791da78423e422026b0ffebf193c05
Dave Moten <davidmoten@gmail.com>
2018-11-07 14:41:46 +1100

    add Maybe test

--- src/test/java/org/davidmoten/kool/MaybeTest.java
+++ src/test/java/org/davidmoten/kool/MaybeTest.java
@@ -1,14 +1,26 @@
 package org.davidmoten.kool;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class MaybeTest {
-    
+public final class MaybeTest {
+
     @Test
     public void testMaybeOf() {
         assertEquals(1, (int) Maybe.of(1).get().get());
     }
-    
+
+    @Test
+    public void testMaybeEmpty() {
+        assertFalse(Maybe.empty().get().isPresent());
+    }
+
+    @Test
+    public void testMaybeEmptyIsSingleton() {
+        assertTrue(Maybe.empty() == Maybe.empty());
+    }
+
 }
