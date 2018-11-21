7dadf52ae8a56390b3492bc9d60e8f2a8b1a3c2c
Dave Moten <davidmoten@gmail.com>
2018-11-21 13:26:50 +1100

    use BaseStreamIterator

--- src/main/java/org/davidmoten/kool/internal/operators/stream/DistinctUntilChanged.java
+++ src/main/java/org/davidmoten/kool/internal/operators/stream/DistinctUntilChanged.java
@@ -1,10 +1,10 @@
 package org.davidmoten.kool.internal.operators.stream;
 
-import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 import org.davidmoten.kool.Stream;
 import org.davidmoten.kool.StreamIterator;
+import org.davidmoten.kool.internal.util.BaseStreamIterator;
 
 import com.github.davidmoten.guavamini.Preconditions;
 
@@ -20,52 +20,27 @@ public final class DistinctUntilChanged<T, K> implements Stream<T> {
 
     @Override
     public StreamIterator<T> iterator() {
-        return new StreamIterator<T>() {
+        return new BaseStreamIterator<T, T>(stream) {
 
-            StreamIterator<T> it = Preconditions.checkNotNull(stream.iterator());
             K key;
-            T next;
 
             @Override
-            public boolean hasNext() {
-                load();
-                return next != null;
-            }
-
-            @Override
-            public T next() {
-                load();
-                T v = next;
-                if (v != null) {
-                    next = null;
-                    return v;
-                } else {
-                    throw new NoSuchElementException();
-                }
-            }
-
-            private void load() {
-                if (it != null && next == null) {
-                    while (it.hasNext()) {
-                        T v = Preconditions.checkNotNull(it.next());
-                        K k = Preconditions.checkNotNull(keySelector.apply(v));
-                        if (!k.equals(key)) {
-                            key = k;
-                            next = v;
-                            break;
-                        }
+            public void load() {
+                while (it.hasNext()) {
+                    T v = Preconditions.checkNotNull(it.next());
+                    K k = Preconditions.checkNotNull(keySelector.apply(v));
+                    if (!k.equals(key)) {
+                        key = k;
+                        next = v;
+                        break;
                     }
                 }
             }
 
             @Override
             public void dispose() {
-                if (it != null) {
-                    it.dispose();
-                    next = null;
-                    key = null;
-                    it = null;
-                }
+                super.dispose();
+                key = null;
             }
 
         };
