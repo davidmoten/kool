dddd5c6778cd2c03e6fe0adc581ee5429da0210b
Dave Moten <davidmoten@gmail.com>
2018-11-22 10:33:02 +1100

    improve coverage

--- src/test/java/org/davidmoten/kool/MaybeTest.java
+++ src/test/java/org/davidmoten/kool/MaybeTest.java
@@ -27,94 +27,109 @@ public final class MaybeTest {
     public void testMaybeEmptyIsSingleton() {
         assertTrue(Maybe.empty() == Maybe.empty());
     }
-    
+
     @Test
     public void testMaybeFlatMap() {
         Maybe.of(1) //
-        .flatMap(x -> Stream.of(x,x)) //
-        .test() //
-        .assertValues(1,1);
+                .flatMap(x -> Stream.of(x, x)) //
+                .test() //
+                .assertValues(1, 1);
     }
-    
+
     @Test
     public void testMaybeFlatMapEmpty() {
         Maybe.<Integer>empty() //
-        .flatMap(x -> Stream.of(x,x)) //
-        .test() //
-        .assertNoValues();
+                .flatMap(x -> Stream.of(x, x)) //
+                .test() //
+                .assertNoValues();
     }
-    
+
     @Test
     public void testMaybeMap() {
         Maybe.of(3).map(x -> x + 1).test().assertValue(4);
     }
-    
+
     @Test
     public void testMaybeMapEmpty() {
         Maybe.<Integer>empty().map(x -> x + 1).test().assertNoValue();
     }
-    
+
     @Test
     public void testMaybeDoOnValue() {
         AtomicInteger b = new AtomicInteger();
         Maybe.of(3).doOnValue(x -> b.set(x)).test().assertValue(3);
         assertEquals(3, b.get());
     }
-    
+
     @Test
     public void testMaybeDoOnValueEmpty() {
         AtomicInteger b = new AtomicInteger();
         Maybe.<Integer>empty().doOnValue(x -> b.set(x)).test().assertNoValue();
         assertEquals(0, b.get());
     }
-    
+
     @Test
     public void testMaybeDoOnEmptyWhenNotEmpty() {
         AtomicBoolean b = new AtomicBoolean();
         Maybe.of(3).doOnEmpty(() -> b.set(true)).test().assertValue(3);
         assertFalse(b.get());
     }
-    
+
     @Test
     public void testMaybeDoOnEmptyWhenEmpty() {
         AtomicBoolean b = new AtomicBoolean();
         Maybe.<Integer>empty().doOnEmpty(() -> b.set(true)).test().assertNoValue();
         assertTrue(b.get());
     }
-    
+
     @Test
     public void testMaybeOrElseWhenNotPresent() {
         Maybe.<Integer>empty().orElse(2).test().assertValue(2);
     }
-    
+
     @Test
     public void testMaybeOrElseWhenPresent() {
         Maybe.of(1).orElse(2).test().assertValue(1);
     }
-    
-    @Test(expected=UncheckedException.class)
+
+    @Test(expected = UncheckedException.class)
     public void testMaybeError() {
         Maybe.error(new IOException("boo")).get();
     }
-    
+
     @Test
     public void testMaybeToStream() {
         Maybe.of(1).toStream().test().assertValuesOnly(1);
     }
-    
+
     @Test
     public void testMaybeToStreamFromEmpty() {
         Maybe.empty().toStream().test().assertNoValuesOnly();
     }
-    
+
     @Test
     public void testMaybeIsPresent() {
         Maybe.of(1).isPresent().test().assertValueOnly(true);
     }
-    
+
     @Test
     public void testMaybeIsPresentWhenEmpty() {
         Maybe.empty().isPresent().test().assertValueOnly(false);
     }
-    
+
+    @Test
+    public void testFromCallable() {
+        Maybe.fromCallable(() -> 1).test().assertValue(1);
+    }
+
+    @Test
+    public void testDoOnError() {
+
+    }
+
+    @Test
+    public void testDefer() {
+
+    }
+
 }
