23ce9596d8544226e20fb8e817b0459b13f6e965
Dave Moten <davidmoten@gmail.com>
2018-11-21 17:06:47 +1100

    update benchmark

--- src/test/java/org/davidmoten/kool/ShakespearePlaysScrabble.java
+++ src/test/java/org/davidmoten/kool/ShakespearePlaysScrabble.java
@@ -27,7 +27,7 @@ import org.openjdk.jmh.annotations.State;
 @State(Scope.Benchmark)
 public class ShakespearePlaysScrabble {
     
-    static final int DURATION_SECONDS = 1;
+    static final int DURATION_SECONDS = 5;
 
     static class MutableLong {
         long value;
