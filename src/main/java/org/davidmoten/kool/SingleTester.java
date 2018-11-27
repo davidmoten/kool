e139e172686b60c221c55e142bd15b5686abd9ba
Dave Moten <davidmoten@gmail.com>
2018-11-27 16:02:21 +1100

    add tests

--- src/main/java/org/davidmoten/kool/SingleTester.java
+++ src/main/java/org/davidmoten/kool/SingleTester.java
@@ -1,5 +1,7 @@
 package org.davidmoten.kool;
 
+import com.github.davidmoten.guavamini.Preconditions;
+
 public final class SingleTester<T> {
 
     private T value;
@@ -7,16 +9,16 @@ public final class SingleTester<T> {
 
     SingleTester(Single<T> single) {
         try {
-        this.value = single.get();
+            this.value = single.get();
         } catch (Throwable e) {
             this.error = e;
         }
     }
-    
+
     public SingleTester<T> assertValue(T v) {
         assertNoError();
         if (!v.equals(value)) {
-            throw new AssertionError("single result "+ value + " not equal to " + v);
+            throw new AssertionError("single result " + value + " not equal to " + v);
         }
         return this;
     }
@@ -24,7 +26,7 @@ public final class SingleTester<T> {
     public SingleTester<T> assertValueOnly(T value) {
         return assertValue(value);
     }
-    
+
     public SingleTester<T> assertNoError() {
         if (error != null) {
             throw new AssertionError("no error expected but one found", error);
@@ -33,20 +35,18 @@ public final class SingleTester<T> {
     }
 
     public SingleTester<T> assertError(Class<? extends Throwable> cls) {
-        if (error == null) {
-            throw new AssertionError("error expected of type " + cls + " but no error was found");
-        } else if (!cls.isInstance(error)) {
+        Preconditions.checkNotNull(cls, "cls cannot be null");
+        if (!cls.isInstance(error)) {
             throw new AssertionError("error expected of type " + cls + " but was of type " + error.getClass());
         }
         return this;
     }
 
     public SingleTester<T> assertErrorMessage(String message) {
-        if (error == null) {
-            throw new AssertionError("error expected but no error was found");
-        } else if (!message.equals(error.getMessage())) {
+        Preconditions.checkNotNull(message, "message cannot be null");
+        if (!message.equals(error.getMessage())) {
             throw new AssertionError("error expected with msg " + message + " but message was: " + error.getMessage());
-        } 
+        }
         return this;
     }
 }
--- src/test/java/org/davidmoten/kool/SingleTesterTest.java
+++ src/test/java/org/davidmoten/kool/SingleTesterTest.java
@@ -1,5 +1,7 @@
 package org.davidmoten.kool;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class SingleTesterTest {
@@ -8,5 +10,35 @@ public class SingleTesterTest {
     public void testNotValue() {
         Single.of(1).test().assertValue(2);
     }
+    
+    @Test(expected=AssertionError.class)
+    public void testNoError() {
+        Single.error(new RuntimeException("boo")).test().assertNoError();
+    }
+    
+    @Test(expected=AssertionError.class)
+    public void testClassErrorIsNot() {
+        Single.error(new RuntimeException("boo")).test().assertError(IOException.class);
+    }
+    
+    @Test
+    public void testClassErrorIs() {
+        Single.error(new RuntimeException("boo")).test().assertError(RuntimeException.class);
+    }
+    
+    @Test(expected=NullPointerException.class)
+    public void testErrorClassNotNull() {
+        Single.error(new RuntimeException("boo")).test().assertError(null);
+    }
 
+    @Test(expected=AssertionError.class)
+    public void testErrorMessageIsNot() {
+        Single.error(new RuntimeException("boo")).test().assertErrorMessage("abc");
+    }
+    
+    @Test
+    public void testErrorMessageIs() {
+        Single.error(new RuntimeException("boo")).test().assertErrorMessage("boo");
+    }
+    
 }
