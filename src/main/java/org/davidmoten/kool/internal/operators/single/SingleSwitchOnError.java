3b13d53d4a88195b9708b4c8f625cf8e4e28403c
Dave Moten <davidmoten@gmail.com>
2018-12-13 11:58:18 +1100

    add null check to function parameter

--- src/main/java/org/davidmoten/kool/internal/operators/single/SingleSwitchOnError.java
+++ src/main/java/org/davidmoten/kool/internal/operators/single/SingleSwitchOnError.java
@@ -3,12 +3,15 @@ package org.davidmoten.kool.internal.operators.single;
 import org.davidmoten.kool.Single;
 import org.davidmoten.kool.function.Function;
 
+import com.github.davidmoten.guavamini.Preconditions;
+
 public final class SingleSwitchOnError<T> implements Single<T> {
 
     private final Single<T> single;
     private final Function<? super Throwable, ? extends Single<? extends T>> function;
 
     public SingleSwitchOnError(Single<T> single, Function<? super Throwable, ? extends Single<? extends T>> function) {
+        Preconditions.checkNotNull(function, "switch function cannot be null");
         this.single = single;
         this.function = function;
     }
