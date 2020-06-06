5e3d9f7d7a194bdfa22dd63404ec4fa2367c94a9
Dave Moten <davidmoten@gmail.com>
2020-06-06 15:54:45 +1000

    add test for README.md

--- kool/src/test/java/org/davidmoten/kool/StatisticsTest.java
+++ kool/src/test/java/org/davidmoten/kool/StatisticsTest.java
@@ -5,23 +5,29 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StatisticsTest {
-    
+
     private static final double PRECISION = 0.0001;
 
     @Test
     public void test() {
-      Statistics s = new Statistics();
-      s = s.add(1).add(2).add(3).add(8);
-      assertEquals(4, s.count());
-      assertEquals(3.5, s.mean(), PRECISION);
-      assertEquals(2.6925824, s.standardDeviation(), PRECISION);
-      assertEquals(7.25, s.variance(), PRECISION);
-      assertEquals(2.16052318, s.kurtosis(), PRECISION);
-      assertEquals(0.9220734033619128, s.skewness(), PRECISION);
-      assertEquals(1, s.min(), PRECISION);
-      assertEquals(8, s.max(), PRECISION);
-      assertEquals(7, s.range(), PRECISION);
-      System.out.println(s.toString("","\n"));
+        Statistics s = new Statistics();
+        s = s.add(1).add(2).add(3).add(8);
+        assertEquals(4, s.count());
+        assertEquals(3.5, s.mean(), PRECISION);
+        assertEquals(2.6925824, s.standardDeviation(), PRECISION);
+        assertEquals(7.25, s.variance(), PRECISION);
+        assertEquals(2.16052318, s.kurtosis(), PRECISION);
+        assertEquals(0.9220734033619128, s.skewness(), PRECISION);
+        assertEquals(1, s.min(), PRECISION);
+        assertEquals(8, s.max(), PRECISION);
+        assertEquals(7, s.range(), PRECISION);
+        System.out.println(s.toString("", "\n"));
     }
-    
+
+    @Test
+    public void testReadme() {
+        Statistics stats = Stream.of(1, 2, 6).statistics(x -> x).get();
+        System.out.println(stats.toString("", "\n"));
+    }
+
 }
